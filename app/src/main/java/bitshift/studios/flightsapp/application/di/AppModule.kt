a6bdb655d597e8bfac6081130d820c7ff605dc00
Xero kun <elo48858@gmail.com>
2023-07-07 18:50:17 +0100

    `FEAT` Setup repositories for dependency injection

--- app/src/main/java/bitshift/studios/flightsapp/application/di/AppModule.kt
+++ app/src/main/java/bitshift/studios/flightsapp/application/di/AppModule.kt
@@ -4,6 +4,8 @@ import android.app.Application
 import androidx.room.Room
 import bitshift.studios.flightsapp.data.db.airport.AirportDatabase
 import bitshift.studios.flightsapp.data.db.bookmarked.BookmarkedFlightsDatabase
+import bitshift.studios.flightsapp.data.repository.BookmarkedFlightDataRepositoryImpl
+import bitshift.studios.flightsapp.data.repository.FlightDataRepositoryImpl
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
@@ -38,5 +40,17 @@ object AppModule {
 			)
 			.fallbackToDestructiveMigration()
 			.build()
+	}
+
+	@Provides
+	@Singleton
+	fun providesFlightDataRepository(db: AirportDatabase): FlightDataRepositoryImpl {
+		return FlightDataRepositoryImpl(db.dao())
+	}
+
+	@Provides
+	@Singleton
+	fun providesBookmarkedFlightDataRepository(db: BookmarkedFlightsDatabase): BookmarkedFlightDataRepositoryImpl {
+		return BookmarkedFlightDataRepositoryImpl(db.dao())
 	}
 }
