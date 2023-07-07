e88f81f3080b88993fe07ea97fc11f65253307fe
Xero kun <elo48858@gmail.com>
2023-07-07 16:33:37 +0100

    `FEAT` Added base application class

---
+++ app/src/main/java/bitshift/studios/flightsapp/application/FlightsApplication.kt
@@ -0,0 +1,7 @@
+package bitshift.studios.flightsapp.application
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class FlightsApplication : Application()
