14d587e65cf230b1b2b8bb77a4afd72a7893221b
Xero kun <elo48858@gmail.com>
2023-07-07 17:20:19 +0100

    `FEAT` Created bookmarked airport database entity

---
+++ app/src/main/java/bitshift/studios/flightsapp/data/db/bookmarked/entities/BookmarkedAirportEntity.kt
@@ -0,0 +1,12 @@
+package bitshift.studios.flightsapp.data.db.bookmarked.entities
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "bookmarked_airports")
+data class BookmarkedAirportEntity(
+	@PrimaryKey
+	val id: Int,
+	val departureCode: String,
+	val arrivalCode: String
+)
