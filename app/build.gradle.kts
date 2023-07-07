96076e2c3a900d9300a13d2f7139af56c0d0d8b9
Xero kun <elo48858@gmail.com>
2023-07-07 15:27:45 +0100

    `BUILD` Setup kapt and other dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,6 +2,8 @@ plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
     id("com.google.devtools.ksp")
+    id("com.google.dagger.hilt.android")
+    kotlin("kapt")
 }
 
 android {
@@ -50,6 +52,10 @@ android {
     }
 }
 
+kapt {
+    correctErrorTypes = true
+}
+
 dependencies {
 
     implementation("androidx.core:core-ktx:1.10.1")
@@ -62,10 +68,23 @@ dependencies {
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.compose.material3:material3")
 
+    // Setup
+    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:${rootProject.extra["viewmodel_compose_version"]}")
+    implementation("androidx.navigation:navigation-compose:${rootProject.extra["navigation_compose_version"]}")
+    implementation("androidx.core:core-splashscreen:${rootProject.extra["splash_version"]}")
+
     // RoomDB
-    implementation("androidx.room:room-ktx:2.5.2")
-    implementation("androidx.room:room-runtime:2.5.2")
-    ksp("androidx.room:room-compiler:2.5.2")
+    implementation("androidx.room:room-ktx:${rootProject.extra["roomdb_version"]}")
+    implementation("androidx.room:room-runtime:${rootProject.extra["roomdb_version"]}")
+    ksp("androidx.room:room-compiler:${rootProject.extra["roomdb_version"]}")
+
+    // Hilt
+    implementation("com.google.dagger:hilt-android:${rootProject.extra["hilt_version"]}")
+    kapt("com.google.dagger:hilt-android-compiler:${rootProject.extra["hilt_version"]}")
+    implementation("androidx.hilt:hilt-navigation-compose:${rootProject.extra["navigation_compose_version"]}")
+
+    // Datastore
+    implementation("androidx.datastore:datastore:${rootProject.extra["datastore_version"]}")
 
     // Testing
     testImplementation("junit:junit:4.13.2")
--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,21 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
+
+buildscript {
+    extra.apply {
+        set("hilt_version", "2.44.2")
+        set("splash_version", "1.0.1")
+        set("roomdb_version", "2.5.2")
+        set("datastore_version", "1.0.0")
+        set("viewmodel_compose_version", "2.6.1")
+        set("navigation_compose_version", "2.6.0")
+        set("navigation_compose_version", "1.0.0")
+    }
+}
+
 plugins {
     id("com.android.application") version "8.0.2" apply false
     id("com.android.library") version "8.0.2" apply false
     id("org.jetbrains.kotlin.android") version "1.7.20" apply false
     id("com.google.devtools.ksp") version "1.8.10-1.0.9" apply false
+    id("com.google.dagger.hilt.android") version "2.44.2" apply false
 }
