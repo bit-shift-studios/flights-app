35f90eef9483b5f81c46160aa24144b31365e240
Xero kun <elo48858@gmail.com>
2023-07-07 19:58:04 +0100

    `FEAT` Updated color definitions

--- app/src/main/java/bitshift/studios/flightsapp/presentation/ui/theme/Color.kt
+++ app/src/main/java/bitshift/studios/flightsapp/presentation/ui/theme/Color.kt
@@ -2,10 +2,8 @@ package bitshift.studios.flightsapp.presentation.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
-
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+val Neutrals50 = Color(0xFFF3F9FC)
+val Main050 = Color(0xFFD9EFFC)
+val Main100 = Color(0xFF6AA8F2)
+val Main250 = Color(0xFF162A3C)
+val Main300 = Color(0xFF09121A)
