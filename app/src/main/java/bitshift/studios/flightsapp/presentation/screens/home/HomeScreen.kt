fa394ef0451c8cbb15183402104d3f607cc47476
Xero kun <elo48858@gmail.com>
2023-07-08 15:51:08 +0100

    `FEAT` Created base search bar

--- app/src/main/java/bitshift/studios/flightsapp/presentation/screens/home/HomeScreen.kt
+++ app/src/main/java/bitshift/studios/flightsapp/presentation/screens/home/HomeScreen.kt
@@ -1,16 +1,25 @@
 package bitshift.studios.flightsapp.presentation.screens.home
 
+import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import bitshift.studios.flightsapp.presentation.ui.components.SearchBar
 
 @Composable
 fun HomeScreen(modifier: Modifier = Modifier) {
-	Scaffold { padding ->
-		Column(modifier = modifier.padding(padding)) {
+	val isDarkTheme = isSystemInDarkTheme()
+
+	Scaffold(
+		topBar = {
+			SearchBar(isDarkTheme = isDarkTheme)
+		},
+	) { padding ->
+		Column(modifier = modifier.padding(padding).padding(horizontal = 16.dp)) {
 			Text(text = "Home Screen")
 		}
 	}
---
+++ app/src/main/java/bitshift/studios/flightsapp/presentation/ui/components/SearchBar.kt
@@ -0,0 +1,59 @@
+package bitshift.studios.flightsapp.presentation.ui.components
+
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextFieldDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import bitshift.studios.flightsapp.presentation.ui.theme.FlightsAppTheme
+import bitshift.studios.flightsapp.presentation.ui.theme.Main050
+import bitshift.studios.flightsapp.presentation.ui.theme.Main250
+import bitshift.studios.flightsapp.presentation.ui.theme.Neutral050
+
+@Composable
+fun SearchBar(
+	modifier: Modifier = Modifier,
+	isDarkTheme: Boolean
+) {
+	val containerColor = when(isDarkTheme) {
+		true -> Main250
+		else -> Main050
+	}
+
+	OutlinedTextField(
+		value = "",
+		onValueChange = {},
+		singleLine = true,
+		textStyle = MaterialTheme.typography.bodyLarge,
+		placeholder = {
+			Text(
+				text = "Search flights",
+				color = if (isDarkTheme) Main050.copy(alpha = 0.5F) else Main250.copy(alpha = 0.7F)
+			)
+		},
+		shape = RoundedCornerShape(16.dp),
+		colors = TextFieldDefaults.outlinedTextFieldColors(
+			containerColor = containerColor,
+			unfocusedBorderColor = containerColor,
+			focusedBorderColor = containerColor,
+			textColor = Neutral050
+		),
+		modifier = modifier
+			.fillMaxWidth()
+			.padding(16.dp)
+	)
+}
+
+@Preview(showBackground = true)
+@Composable
+fun SearchBarPreview() {
+	FlightsAppTheme {
+		SearchBar(isDarkTheme = false)
+	}
+}
