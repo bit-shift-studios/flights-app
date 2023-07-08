e62f0795c5de90764ae697e8355602d7a9f488c0
Xero kun <elo48858@gmail.com>
2023-07-08 16:01:51 +0100

    `FEAT` Quality of code updates

--- app/src/main/java/bitshift/studios/flightsapp/presentation/ui/components/SearchBar.kt
+++ app/src/main/java/bitshift/studios/flightsapp/presentation/ui/components/SearchBar.kt
@@ -10,7 +10,6 @@ import androidx.compose.material3.Text
 import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -19,7 +18,6 @@ import bitshift.studios.flightsapp.presentation.ui.theme.FlightsAppTheme
 import bitshift.studios.flightsapp.presentation.ui.theme.Main050
 import bitshift.studios.flightsapp.presentation.ui.theme.Main100
 import bitshift.studios.flightsapp.presentation.ui.theme.Main250
-import bitshift.studios.flightsapp.presentation.ui.theme.Main300
 import bitshift.studios.flightsapp.presentation.ui.theme.Neutral050
 
 @Composable
