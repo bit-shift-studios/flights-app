19be9a702dd9e334277699297e4fe0a2102410dc
Xero kun <elo48858@gmail.com>
2023-07-08 16:00:18 +0100

    `FEAT` Added search icon to search bar

--- app/src/main/java/bitshift/studios/flightsapp/presentation/ui/components/SearchBar.kt
+++ app/src/main/java/bitshift/studios/flightsapp/presentation/ui/components/SearchBar.kt
@@ -3,17 +3,23 @@ package bitshift.studios.flightsapp.presentation.ui.components
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.toArgb
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import bitshift.studios.flightsapp.R
 import bitshift.studios.flightsapp.presentation.ui.theme.FlightsAppTheme
 import bitshift.studios.flightsapp.presentation.ui.theme.Main050
+import bitshift.studios.flightsapp.presentation.ui.theme.Main100
 import bitshift.studios.flightsapp.presentation.ui.theme.Main250
+import bitshift.studios.flightsapp.presentation.ui.theme.Main300
 import bitshift.studios.flightsapp.presentation.ui.theme.Neutral050
 
 @Composable
@@ -31,6 +37,13 @@ fun SearchBar(
 		onValueChange = {},
 		singleLine = true,
 		textStyle = MaterialTheme.typography.bodyLarge,
+		leadingIcon = {
+			Icon(
+				painter = painterResource(id = R.drawable.icon_search),
+				contentDescription = "search",
+				tint = Main100
+			)
+		},
 		placeholder = {
 			Text(
 				text = "Search flights",
@@ -50,10 +63,24 @@ fun SearchBar(
 	)
 }
 
-@Preview(showBackground = true)
+@Preview(
+	showBackground = true,
+	backgroundColor = 0xFFF3F9FC
+)
 @Composable
-fun SearchBarPreview() {
+fun SearchBarLightPreview() {
 	FlightsAppTheme {
 		SearchBar(isDarkTheme = false)
+	}
+}
+
+@Preview(
+	showBackground = true,
+	backgroundColor = 0xFF09121A
+)
+@Composable
+fun SearchBarDarkPreview() {
+	FlightsAppTheme {
+		SearchBar(isDarkTheme = true)
 	}
 }
