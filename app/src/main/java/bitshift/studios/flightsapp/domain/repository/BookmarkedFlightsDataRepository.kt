ed304b263848a38051f4a9f74572d00c9633b382
Xero kun <elo48858@gmail.com>
2023-07-12 20:09:59 +0100

    `CHORE` Renamed bookmarked airport entity to bookmarked flight entity

--- app/src/main/java/bitshift/studios/flightsapp/domain/repository/BookmarkedFlightsDataRepository.kt
+++ app/src/main/java/bitshift/studios/flightsapp/domain/repository/BookmarkedFlightsDataRepository.kt
@@ -1,11 +1,11 @@
 package bitshift.studios.flightsapp.domain.repository
 
-import bitshift.studios.flightsapp.data.db.bookmarked.entities.BookmarkedAirportEntity
+import bitshift.studios.flightsapp.data.db.bookmarked.entities.BookmarkedFlightEntity
 import kotlinx.coroutines.flow.Flow
 
 interface BookmarkedFlightsDataRepository {
-	fun getBookmarkedAirports(): Flow<List<BookmarkedAirportEntity>>
+	fun getBookmarkedAirports(): Flow<List<BookmarkedFlightEntity>>
 	suspend fun isAirportBookmarked(code: String): Boolean
-	suspend fun addAirportToBookmarks(airport: BookmarkedAirportEntity)
-	suspend fun removeAirportFromBookmarks(airport: BookmarkedAirportEntity)
+	suspend fun addAirportToBookmarks(airport: BookmarkedFlightEntity)
+	suspend fun removeAirportFromBookmarks(airport: BookmarkedFlightEntity)
 }
