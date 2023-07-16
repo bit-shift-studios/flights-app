cc41f12b3d6bcbba1b68c02f21469a0df6e70224
Xero kun <elo48858@gmail.com>
2023-07-16 21:07:49 +0100

    `BUILD` Quality of code updates

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -24,7 +24,7 @@ android {
     }
 
     buildTypes {
-        release {
+        getByName("release") {
             isMinifyEnabled = false
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
