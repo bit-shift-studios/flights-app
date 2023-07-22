1e9582291935bd752a92ffedc2ae6cc98c140c44
Xero kun <elo48858@gmail.com>
2023-07-22 22:32:48 +0100

    `BUGFIX` Corrected error in flight data map entries

--- app/src/main/java/bitshift/studios/flightsapp/data/FlightsData.kt
+++ app/src/main/java/bitshift/studios/flightsapp/data/FlightsData.kt
@@ -3,14 +3,14 @@ package bitshift.studios.flightsapp.data
 val FlightsDataMap = hashMapOf(
 	"OPO" to listOf("WAW", "ARN", "MUC", "LIS", "TXL"),
 	"WAW" to listOf("OPO", "AGP", "HAM", "MAD", "ALC"),
-	"MAD" to listOf("BGY", "SOY", "ZRH", "ALC"),
+	"MAD" to listOf("BGY", "SOF", "ZRH", "ALC"),
 	"ARN" to listOf("TXL", "WAW", "HAM", "MUC", "BGY", "OPO"),
 	"TXL" to listOf("OSL", "BSL", "FCO", "LYS", "ATH", "KEF", "HER"),
-	"HER" to listOf("SOY", "ZRH", "TXL", "CPH", "LIS", "ALC", "AGP", "MAD"),
+	"HER" to listOf("SOF", "ZRH", "TXL", "CPH", "LIS", "ALC", "AGP", "MAD"),
 	"BGY" to listOf("FCO", "BSL", "LIS", "MUC", "WAW", "ARN", "OPO", "MAD"),
 	"MRS" to listOf("TXL", "BSL", "DUB", "MUC", "DUS", "HER", "OPO", "MAD"),
 	"DUB" to listOf("ARN", "ATH", "DUS", "TXL", "SVO"),
-	"DUS" to listOf("SOY", "ATH", "ZRH", "HER", "DUB", "OPO", "KEF", "MAD"),
+	"DUS" to listOf("SOF", "ATH", "ZRH", "HER", "DUB", "OPO", "KEF", "MAD"),
 	"BCN" to listOf("FRA", "GRA", "ZRH", "HAM", "BRU", "MUC", "ALC", "LYS"),
 	"LYS" to listOf("FCL", "OSL", "VIE", "ORN", "SOF", "MUC", "KEF", "LIS"),
 	"FCO" to listOf("FCL", "BSL", "HAM", "ORN", "VIE", "LIS"),
@@ -18,5 +18,5 @@ val FlightsDataMap = hashMapOf(
 	"BSL" to listOf("WAW", "ATH", "CDG", "ALC", "VIE", "FCO", "AMS", "CPH"),
 	"HEL" to listOf("TXL", "AGP", "ARN", "ALC", "LYS", "FCO", "AMS", "CPH", "DUB"),
 	"OSL" to listOf("TXL", "KEF", "ZRH", "ALC", "LYS", "LIS", "FCO", "ALC", "DUS"),
-	"ALC" to listOf("AGP", "SOY", "BGY", "BCN", "OPO", "GRA", "OSL", "VIE")
+	"ALC" to listOf("AGP", "SOF", "BGY", "BCN", "OPO", "GRA", "OSL", "VIE")
 )
